ff16b3c0ebd88cdb18fd516d0c734ca6b1f556c2
Andrey Piarouski <EstSkif@gmail.com>
2023-02-01 20:56:25 +0400

    refactor: add output type

--- src/Utils/createLinkElement.ts
+++ src/Utils/createLinkElement.ts
@@ -1,4 +1,4 @@
-export default function createLinkElement(link: string, className?: string) {
+export default function createLinkElement(link: string, className?: string): HTMLAnchorElement {
     const element = document.createElement('a');
     element.href = link;
     element.classList.add('link');
