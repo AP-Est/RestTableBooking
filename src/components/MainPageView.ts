c57bbc5a1f08d9f36f85fa6dc3032f0e3af650b0
Natallia Siniak <natalia.vasilevna.s@gmail.com>
2023-02-03 16:50:49 +0400

    feat: add additions in header

--- src/components/MainPageView.ts
+++ src/components/MainPageView.ts
@@ -5,15 +5,34 @@ import createHeader from '../templates/createHeader';
 
 export class MainPageView {
     body: HTMLElement;
-    div: HTMLElement;
     header: HTMLElement;
 
     constructor() {
         this.body = getElement('body') as HTMLElement;
         this.body.innerHTML = '';
-        this.div = createElement('div', 'div-test');
 
-        this.header = createHeader();
-        this.body.append(this.header, this.div);
+        this.header = this.displayHeader();
+        this.body.append(this.header);
+    }
+
+    displayHeader() {
+        const header = createElement('div', 'header');
+        header.classList.add('header__wrapper');
+
+        const headerTop = createElement('div', 'header-top');
+        headerTop.classList.add('header-top__wrapper');
+        const headerTopLogo = createElement('div', 'header-top-logo');
+        const headerTopMain = createElement('div', 'header-top-main');
+        const headerTopMenu = createElement('div', 'header-top-menu');
+        const headerTopReviews = createElement('div', 'header-top-reviews');
+        const headerTopTel = createElement('div', 'header-top-tel');
+
+        const headerMain = createElement('div', 'header-main');
+        headerMain.classList.add('header-main__wrapper');
+
+        headerTopMain.append(headerTopMenu, headerTopReviews);
+        headerTop.append(headerTopLogo, headerTopMenu, headerTopTel);
+        header.append(headerTop, headerMain);
+        return header;
     }
 }
