e483ad4e6ed5aa4be0c8b9d6ef609dcfc9550470
Andrey Piarouski <EstSkif@gmail.com>
2023-02-10 04:50:52 +0400

    feat: add sizer to hall block

--- src/templates/reservation/createHallBlock.ts
+++ src/templates/reservation/createHallBlock.ts
@@ -3,6 +3,8 @@ import createElement from '../../Utils/createElement';
 import { renderTable } from '../../Utils/renderTable';
 
 export default function createHallBlock(hallView: ITableState[]) {
+    const windowOuterWidth = window.outerWidth;
+    const windowOuterHeight = window.outerHeight;
     const wrapper = createElement('div', 'hall__wrapper');
     /*
     //const colorT = '#B0ADAD';
@@ -22,6 +24,13 @@ export default function createHallBlock(hallView: ITableState[]) {
         const table = createElement('div', `hall__table${hallView[i].tableNumber}`, 'hall__allTables');
         wrapper.appendChild(table);
         table.innerHTML = renderTable(hallView[i].tableType, hallView[i].tableColor, hallView[i].sitColor);
+    }
+    if (windowOuterWidth > windowOuterHeight) {
+        wrapper.style.width = '75vh';
+        wrapper.style.height = '75vh';
+    } else {
+        wrapper.style.width = '75vw';
+        wrapper.style.height = '75vw';
     }
     return wrapper;
 }
