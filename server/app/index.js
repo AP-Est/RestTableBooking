d87e39d897980603508022c03e9b1528673395b6
Viktoriya Danilova <v_kozulya@mail.ru>
2023-02-21 20:38:01 +0500

    refactor: add new styles to booking and reviews page and remove deprecated code

--- server/app/index.js
+++ server/app/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const cookieOptions = {
     httpOnly: true,
 };
